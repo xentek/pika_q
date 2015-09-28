9e75ce778127a056028db9f9846f316c43a81478
Eric Marden <eric.marden@gmail.com>
2015-09-28 12:36:09 -0500

    set default readtimeout and heartbeat options

--- lib/pika_q/connection.rb
+++ lib/pika_q/connection.rb
@@ -4,8 +4,8 @@ module PikaQ
   class Connection
     attr_reader :channel, :rabbitmq_url, :session
 
-    def initialize(rabbitmq_url = ENV['RABBITMQ_URL'])
-      @session = Bunny.new(rabbitmq_url)
+    def initialize(rabbitmq_url = ENV['RABBITMQ_URL'], options = { read_timeout: 10, heartbeat: 10 })
+      @session = Bunny.new(rabbitmq_url, options)
       @session.start
       @channel = @session.create_channel
     end
